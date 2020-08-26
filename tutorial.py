840fd1ad7f8c06bc3311ba574093408f87561b6b
Felix Chen <felixchen20@gmail.com>
2020-08-26 01:03:17 -0700

    initial commit

---
+++ tutorial.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
